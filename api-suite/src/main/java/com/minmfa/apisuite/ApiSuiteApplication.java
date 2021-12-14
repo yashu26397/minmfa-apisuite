df44bf803d56c5f06e0a101d77c0c5248bea8c3b
yashu26397 <49430763+yashu26397@users.noreply.github.com>
2021-12-14 16:09:08 +0530

    Created API suite for minmfa

---
+++ api-suite/src/main/java/com/minmfa/apisuite/ApiSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.minmfa.apisuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSuiteApplication.class, args);
+	}
+
+}
---
+++ api-suite/src/test/java/com/minmfa/apisuite/ApiSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minmfa.apisuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
